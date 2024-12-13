5c65a3f3b6fdc2a0e60f9506e5d8656ba22ceea0
GabrieldeSouzaGomes <166565269+GabrieldeSouzaGomes@users.noreply.github.com>
2024-12-12 23:27:14 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Filmes from './components/Filmes'
+function App() {
+  
+
+  return (
+    <div className="App"> 
+    <Filmes/>
+    </div>
+
+  );  
+  
+}
+
+
+export default App
